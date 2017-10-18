f58ca6077868fa711f3b1c80763ae73a18209179
HenryZHLZhang <774805244@qq.com>
2017-10-18 09:04:27 +0800

    上线版本

--- src/main/java/com/henry/example/bean/MysqlService.java
+++ src/main/java/com/henry/example/bean/MysqlService.java
@@ -1,5 +1,7 @@
 package com.henry.example.bean;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +20,58 @@ public class MysqlService {
 	public void insertSQL(String name,String info){
 		jdbcTemplate.update("INSERT INTO menu (name, info) VALUES ('"+name+"', '"+info+"');");
 //		jdbcTemplate.update("SELECT name FROM auth_group;");
+	}
+	
+	public String insertUser(String account,String password,String name){
+		String resultString;
+		try {			
+			jdbcTemplate.update("INSERT INTO bdm291580582_db.user (account, password, name) VALUES ('"+account+"', '"+password+"', '"+name+"');");
+			resultString = "True";
+		} catch (Exception e) {
+			// TODO: handle exception
+			resultString = "False";
+		}
+		String jsonResult = resultString;
+        JSONObject result = new JSONObject();
+		result.accumulate("data", jsonResult);
+		result.accumulate("msg", "请求成功");
+		result.accumulate("success", true);
+		return result.toString();
+	}
+	
+	public String insertComment(String id,String uid,String comment){
+		String resultString;
+		SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date dateObj = new Date();
+		String date  = dateFormater.format(dateObj);
+		try {			
+			jdbcTemplate.update("INSERT INTO bdm291580582_db.comment (id, uid, comment, date) VALUES ('"+id+"', '"+uid+"', '"+comment+"', '"+date+"');");
+			resultString = "True";
+		} catch (Exception e) {
+			// TODO: handle exception
+			resultString = "False";
+		}
+		String jsonResult = resultString;
+        JSONObject result = new JSONObject();
+		result.accumulate("data", jsonResult);
+		result.accumulate("msg", "请求成功");
+		result.accumulate("success", true);
+		return result.toString();
+	}
+	
+	public String searchUser(String account){
+		List<Map<String, Object>> getsql = jdbcTemplate.queryForList("SELECT * FROM bdm291580582_db.user WHERE account LIKE '%" + account + "%';");
+		return getsql.get(0).get("password").toString() + "," + getsql.get(0).get("name").toString() + "," + getsql.get(0).get("uid").toString();
+	}
+	
+	public String searchComment(String id) {
+		List<Map<String, Object>> getsql = jdbcTemplate.queryForList("SELECT comment.comment, comment.date, user.name FROM comment INNER JOIN user ON comment.uid = user.uid AND comment.id = "+ id +" ORDER BY comment.date; ");
+		JSONArray jsonResult = JSONArray.fromObject(getsql);
+		JSONObject result = new JSONObject();
+		result.accumulate("data", jsonResult);
+		result.accumulate("msg", "请求成功");
+		result.accumulate("success", true);
+		return result.toString();
 	}
 	
 	public String getSQL(String dish_name) {
--- src/main/java/com/henry/example/controller/MainController.java
+++ src/main/java/com/henry/example/controller/MainController.java
@@ -9,7 +9,6 @@ import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,9 +19,8 @@ import com.henry.example.bean.MysqlService;
 import com.henry.example.bean.RedisService;
 import com.henry.example.bean.User;
 
-import javassist.expr.NewArray;
+import net.sf.json.JSONObject;
 
-import com.henry.example.bean.JsontoXML;;
 
 @Controller
 public class MainController {
@@ -30,7 +28,7 @@ public class MainController {
 	private RedisService redisServer;
 	
 	@Autowired 
-	private MysqlService postgresqlService;
+	private MysqlService mysqlService;
 	
 	
 	@RequestMapping(value="/hello/{name}",method = RequestMethod.GET)
@@ -48,25 +46,52 @@ public class MainController {
 	@ResponseBody 
 	@RequestMapping(value = "/getMenu")
 	public String getMenu() {
-	    return postgresqlService.getSQLAll();
+	    return mysqlService.getSQLAll();
 	}
 	
 	@ResponseBody
-	@RequestMapping(value="search/{keyword}")
+	@RequestMapping(value="/search/{keyword}")
 	public String search(@PathVariable String keyword){
-		return postgresqlService.getSQL(keyword);
+		return mysqlService.getSQL(keyword);
 	}
 	
 	@ResponseBody
-	@RequestMapping(value="/xsdtree/request",method = RequestMethod.POST)
-	public String savePost(HttpServletRequest request){
-		String input_json = request.getParameter("input_json");
-//		JsontoXML jsontoXML = new JsontoXML();
-//		String xml_str = jsontoXML.conversion(input_json);
-		System.out.println(input_json);
-//		postgresqlService.insertSQL("henry_xsd_01", input_json);
-		return "Commit success!";
-	}
+	@RequestMapping(value = "/login/{account}&{password}")  
+    public String login(@PathVariable String account, @PathVariable String password) {  
+		String pwNameId = mysqlService.searchUser(account);
+        String jsonResult;
+		if (password.equals(pwNameId.split(",")[0])) {  
+			jsonResult = "[{\"uid\":"+pwNameId.split(",")[2]+",\"name\":\""+pwNameId.split(",")[1]+"\"}]";
+        } else {  
+        	jsonResult = "False";
+        }  
+        JSONObject result = new JSONObject();
+		result.accumulate("data", jsonResult);
+		result.accumulate("msg", "请求成功");
+		result.accumulate("success", true);
+		return result.toString();
+    }  
+	
+	@ResponseBody
+	@RequestMapping(value = "/register/{account}&{password}&{name}")  
+    public String register(@PathVariable String account, @PathVariable String password, @PathVariable String name) {  
+		String result = mysqlService.insertUser(account, password, name);
+		return result;
+    }  
+	
+	@ResponseBody
+	@RequestMapping(value = "/comment/{id}&{uid}&{comment}")  
+    public String comment(@PathVariable String id, @PathVariable String uid, @PathVariable String comment) {  
+		String result = mysqlService.insertComment(id, uid, comment);
+		return result;
+    }
+	
+	@ResponseBody
+	@RequestMapping(value = "/searchComment/{id}")  
+    public String searchComment(@PathVariable String id) {  
+		String menu = mysqlService.searchComment(id);
+		return menu;
+    }
 	
 	@ResponseBody
 	@RequestMapping(value="post",method = RequestMethod.POST)
@@ -81,7 +106,7 @@ public class MainController {
 		map.put("name", name);
 		map.put("value", "hello world");
 //		redisServer.save(name, "hello world");
-		postgresqlService.insertSQL(name, "hello world");
+		mysqlService.insertSQL(name, "hello world");
 		return map;
 	}
 	
