5a6a02269e3508c46263c3ea5b709f0c6e809948
HenryZHLZhang <774805244@qq.com>
2017-10-30 08:57:54 +0800

    删除部分无用代码

--- src/main/java/com/henry/example/controller/MainController.java
+++ src/main/java/com/henry/example/controller/MainController.java
@@ -1,22 +1,13 @@
 package com.henry.example.controller;
 
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.henry.example.bean.MysqlService;
-import com.henry.example.bean.RedisService;
-import com.henry.example.bean.User;
 
 import net.sf.json.JSONObject;
 
